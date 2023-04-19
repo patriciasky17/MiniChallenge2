c6039faaf95710ebc03b9f26ca0c293047911649
Patricia Ho <hopatricia1711@gmail.com>
2023-04-19 15:53:06 +0700

    Initial Commit

---
+++ MiniChallenge1/MiniChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge1App.swift
+//  MiniChallenge1
+//
+//  Created by Patricia Ho on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
