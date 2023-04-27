b460740560107a5203a405ad2d69d63e3e3a90c9
Joshua <joshuatony7@gmail.com>
2023-04-28 00:53:19 +0700

    Add History Card View and View

---
+++ MiniChallenge1/Core/Themes/Presenters/Components/HistoryJournalCardView.swift
@@ -0,0 +1,54 @@
+//
+//  HistoryJournalCardView.swift
+//  MiniChallenge1
+//
+//  Created by Joshua on 27/04/23.
+//
+
+import SwiftUI
+
+struct HistoryJournalCardView: View {
+    @State var lastEdited = "May 27th, 2022"
+    
+    var body: some View {
+        NavigationLink (destination: HistoryView()){
+                VStack {
+                    VStack (spacing: 10) {
+                        HStack {
+                            Text("Psychopatic Journal")
+                                .font(.largeTitle)
+                                .fontWeight(.medium)
+                            .foregroundColor(AppColor.neutral70)
+                            .multilineTextAlignment(.leading)
+                            Spacer()
+                        }
+                        HStack {
+                            Text("Last Edited \(lastEdited)")
+                                .foregroundColor(AppColor.green100)
+                                .multilineTextAlignment(.leading)
+                            Spacer()
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .frame(width: .infinity, height: 200)
+                .padding(25).padding(.top, 20)
+                .background(
+                    Rectangle()
+                        .foregroundColor(AppColor.neutral10)
+                        .frame(width: .infinity, height: 200)
+                )
+                .cornerRadius(30)
+        }
+    }
+}
+
+struct HistoryJournalCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryJournalCardView()
+    }
+}
+
+
--- MiniChallenge1/Features/Journaling/Presenters/Components/HistoryView.swift
+++ MiniChallenge1/Features/Journaling/Presenters/Components/HistoryView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct HistoryView: View {
+    @State var items = [
+        HistoryJournalCardView(),
+        HistoryJournalCardView(),
+        HistoryJournalCardView(),
+        HistoryJournalCardView()
+    ]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text("Heyy It's History View")
+        VStack {
+            HStack {
+                Text("History")
+                    .multilineTextAlignment(.leading)
+                    .font(.largeTitle)
+                Spacer()
+            }
+            
+//            ScrollView {
+                VStack {
+                    VStack{
+                        List{
+                            ForEach(0..<4){ index in
+                                items[index]                        .swipeActions {
+                                    Button (role: .destructive) {
+                                                                    print("Edit button was tapped")
+                                                                } label: {
+                                                                    Image(systemName: "minus.circle.fill")
+                                                                }
+                                }
+                                .listRowSeparator(.hidden)
+                                .listRowBackground(
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .background(.clear)
+                                        .foregroundColor(.clear)
+//                                        .padding(
+//                                            EdgeInsets(
+//                                                top: 2,
+//                                                leading: 10,
+//                                                bottom: 2,
+//                                                trailing: 10
+//                                            )
+//                                        )
+                                )
+                            }
+                            .onDelete{ IndexSet in
+                                items.remove(atOffsets: IndexSet)
+                            }
+                        }
+                    }
+                }
+            
+                
+//            }
+                
+            Spacer()
+        }
+        .padding([.leading, .trailing, .top], 40)
+        .background(AppColor.neutral20)
     }
 }
 
