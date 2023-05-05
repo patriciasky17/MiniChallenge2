1223e8b2fa25d3d4c3edee46e96b75ecb577a9cd
Joshua <joshuatony7@gmail.com>
2023-05-05 14:18:49 +0700

    Add file for model and viewmodel

---
+++ MiniChallenge1/Features/SelfAssessment/Models/AsssessmentModel.swift
@@ -0,0 +1,8 @@
+//
+//  AsssessmentModel.swift
+//  MiniChallenge1
+//
+//  Created by Joshua on 05/05/23.
+//
+
+import Foundation
---
+++ MiniChallenge1/Features/SelfAssessment/ViewModels/AssessmentViewModels.swift
@@ -0,0 +1,9 @@
+//
+//  AssessmentViewModels.swift
+//  MiniChallenge1
+//
+//  Created by Joshua on 05/05/23.
+//
+
+import Foundation
+
